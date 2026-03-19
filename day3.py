75581f2183e5ebc8ea10c261cb33367473967253
AKASH <157004421+REDMI-9-PRIME@users.noreply.github.com>
2026-03-19 14:52:01 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,16 +1,34 @@
-name = input("Enter your name:")
-marks = int(input("Enter your marks: "))
+#Case study on Grade Calculator
 
-if marks >= 90:
+# Student Details
+student_name = input("Enter Student Name:")
+student_id = input("Enter Student ID:")
+
+#Marks Input
+s1 = float(input("Enter Subject 1  Marks:"))
+s2 = float(input("Enter Subject 2  Marks:"))
+s3 = float(input("Enter Subject 3  Marks:"))
+s4 = float(input("Enter Subject 4  Marks:"))
+s5 = float(input("Enter Subject 5  Marks:"))
+
+#Calculation
+total = s1 + s2 + s2 + s4 + s5
+percentage = total / 5
+
+#Grade Logic
+if percentage >= 90:
     grade = "A"
-elif marks >= 75:
+elif percentage >= 75:
     grade = "B"
-elif marks >= 50:
-    grade = "C"
+elif percentage >= 50:
+    grade  = "C"
 else:
     grade = "Fail"
     
-    print("\n--- Result ---")
-    print("Name:",name)
-    print("Marks:",marks)
-    print("Grade:",grade)
+# Output
+print("\n--- Result ---")
+print("Name:", student_name)
+print("ID:",student_id)
+print("Total:", total)
+print("Percentage:", percentage)
+print("Grade:", grade)

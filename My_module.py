5f6c43d38bb78c69d6b88ba4ba9d6dfeafb98909
REDMI-9-PRIMEAKASH <narasimhaakash7799@gmail.com>
2026-04-14 12:56:42 +0530

    My_module.py

---
+++ My_module.py
@@ -0,0 +1,23 @@
+num = 7
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+
+name = "Python module"
+
+'''def prime_num(num,count):
+    for j in range(1,num+1):
+        if num % j == 0:
+            count+= 1
+    if count == 2:
+        print(f"{num} is a prime")
+    else:
+        print(f"{num} is not a prime")
+prime_num(num=9,count=0)
+prime_num(num=47,count=0)
+'''

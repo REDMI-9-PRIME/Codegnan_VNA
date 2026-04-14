a95492f2ffbead643103f12524f97eabab152a8e
REDMI-9-PRIMEAKASH <narasimhaakash7799@gmail.com>
2026-04-14 12:53:39 +0530

    day21.py

---
+++ day21.py
@@ -0,0 +1,47 @@
+'''Modules:
+----------
+-->A module in python is a simple file that contains python code(functions,variable,classes).
+-->To use modules, we have to use a keyword called import before the module.
+
+Types of Modules:
+----------------
+1. built-in or inbuilt , 2. user-define modules
+
+import My_module
+print(My_module.add(6,7))
+print(My_module.sub(6,7))
+print(My_module.multiply(6,7))
+print(My_module.divide(6,7))
+
+import day17
+print(day17.prime_num(9,0))
+
+import My_module
+print(My_module.prime_num(9,0))
+
+user-define modules:
+-------------------
+-->This is developed by the user or programmer inside a file of python code and used by
+called import with filename..
+syntax:  import(keyword) file_name
+------   file_name.functionality
+
+import My_module
+print(My_module.num+8)
+
+import My_module
+print(My_module.num-7)
+
+built-in or inbuilt:
+-------------------
+-->Already these are comes with installation and they are ready to use in the program.
+-->This is developed by the developer.
+example: OS,Math
+syntax:   import(keyword) module_name
+------    module_name.functionality
+
+import math
+print(math.sqrt(25))
+'''
+
+    

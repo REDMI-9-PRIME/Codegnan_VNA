19ef7dca14c9d4d8fbeb051bdea59367de0b03d4
REDMI-9-PRIMEAKASH <narasimhaakash7799@gmail.com>
2026-04-29 12:53:00 +0530

    day33.py

---
+++ day33.py
@@ -0,0 +1,66 @@
+'''-------------------------------DATA ANALYSIS-------------------------------------------
+This i critical bcoz it converts raw data into actionable insights,enabling information to
+decision-making easy and improve operational efficiency...
+
+1.Decision-making
+2.improved operational Efficiency
+3.Customer understanding
+4.Market Insights
+5.Risk management
+6.data-driven strategies
+
+*pip install matplotlib
+
+line plot:
+---------
+import matplotlib.pyplot as pit
+x = [1,2,3,4,5]
+y = [10,20,15,25,9]
+pit.plot(y,x)
+pit.show()
+
+Bar graph:
+---------
+import matplotlib.pyplot as pit
+pit.bar(["TV9","NDTV","SAKSHITV"],[4,7,6])
+pit.show()
+
+Pie Graph:
+---------
+import matplotlib.pyplot as pit
+pit.pie([30,30,20,20], labels = ["Akash","Kartheek","sai","dude"])
+pit.show()
+
+Histogram:
+---------
+import matplotlib.pyplot as pit
+pit.hist([23,15,78,12])
+pit.show()
+
+Numpy:
+-----
+-->Numpy(Numerical python) is the foundational open-source library for scientific
+computing in python, providing high-performance,N-dimensional array objects(ndarray)
+-->This enables efficient numerical computation linear algebra, and data manipulation,
+serving as the basis for the tools like Tensorflow and scipy.
+
+ pip install numpy and pandas
+
+import numpy as np
+arr = np.array([1,2,3])
+print(arr - 1)
+
+Pandas:
+------
+-->This pandas is used for handling structured data in table format
+
+pip install pandas
+
+import pandas as pd
+data = {"Name" : ["Akash","Kartheek"],"Marks" : [56,78]}
+any = pd.DataFrame(data)
+print(any)
+'''
+
+
+
